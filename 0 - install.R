c1a290b72229af3bbf2ab04971b5ddfc8276aa77
Ronaldo Padula <ronaldopadula@gmail.com>
2024-09-29 03:21:56 -0300

    Update 0 - install.R
    Código de verificarção de biblioteca instalada ou não. Se não, instalar!

--- 0 - install.R
+++ 0 - install.R
@@ -5,11 +5,19 @@
 # OBS: o SQLite deve estar instalado no seu computador
 # consultar o site do projeto: https://www.sqlite.org/
 
-install.packages("rvest", dependencies = TRUE)
-install.packages("tidyverse", dependencies = TRUE)
-install.packages("plyr", dependencies = TRUE) 
-install.packages("RSQLite", dependencies = TRUE)
-install.packages("DBI", dependencies = TRUE)
-install.packages("progress", dependencies = TRUE)
+# Verificar bibliotecas e instalar se precisar:
+
+list.of.packages <- c("rvest","tidyverse","plyr","RSQLite","DBI","progress")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
+
+# Ou instalar uma a uma
+
+#install.packages("rvest", dependencies = TRUE)
+#install.packages("tidyverse", dependencies = TRUE)
+#install.packages("plyr", dependencies = TRUE) 
+#install.packages("RSQLite", dependencies = TRUE)
+#install.packages("DBI", dependencies = TRUE)
+#install.packages("progress", dependencies = TRUE)
 
 #Obs: cara arquivo do programa terá a chamada das bibliotecas para executar.
